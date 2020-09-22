98272899551cf102801f61c64ca266ec18fd5648
navic1992 <civan.erbay@web.de>
2020-09-22 12:35:28 +0200

    Add AddTodo Component

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -11,14 +11,26 @@ const getters = {
 };
 
 const actions = {
+
+
     async fetchTodos({ commit }) {          //We commit a mutation
-        const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
         commit('setTodos', response.data) //first param = mutation we want to call, second param = what we want to pass in
+    },
+
+
+    async addTodo({ commit }, title) {
+        const response = await axios.post('https://jsonplaceholder.typicode.com/todos', {
+            title, completed: false
+        });
+
+        commit('newTodo', response.data); //first param = mutation we want to call, second param = what we want to pass in
     }
 };
 
 const mutations = {
-    setTodos: (state, todos) => (state.todos = todos )
+    setTodos: (state, todos) => state.todos = todos,
+    newTodo: (state, todo) => state.todos.unshift(todo)
 }; 
 
 export default {
