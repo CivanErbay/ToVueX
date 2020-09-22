aa865fb3c8aeb48c699e3d168550aaa256afc965
navic1992 <civan.erbay@web.de>
2020-09-22 16:00:32 +0200

    Add updateTodo-Method

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
     <h3>Todos</h3>
+    <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span>
+        <span class="incomplete-box"></span> = Incomplete
+      </span>
+      <span>
+        <span class="complete-box"></span> = Complete
+      </span>
+    </div>
     <div class="todos">
-      <div v-for="todo in allTodos" v-bind:key="todo.id" class="todo">
+      <div @dblclick="onDblClick(todo)" v-for="todo in allTodos" v-bind:key="todo.id" class="todo" v-bind:class="{'is-complete':todo.completed}">
         <p>{{todo.title}}</p>
         <img @click="deleteTodo(todo.id)" class="todos__delete-icon" src="../assets/delete.svg" alt="">
       </div>
@@ -16,7 +25,16 @@ import { mapGetters, mapActions } from "vuex"; //This gets the getter and action
 export default {
   name: "Todos",
   methods: {
-    ...mapActions(["fetchTodos", "deleteTodo"]),
+    ...mapActions(["fetchTodos", "deleteTodo", "updateTodo"]),
+    onDblClick(todo) {
+      const updTodo = {
+        id: todo.id,
+        title: todo.title,
+        completed: !todo.completed,
+      }
+
+      this.updateTodo(updTodo)
+    }
   },
   computed: mapGetters(["allTodos"]),
   created() {
@@ -48,5 +66,31 @@ export default {
     right: 10px;
     color: #fff;
     cursor: pointer;
+}
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+.is-complete {
+  background: #35495e;
+  color: #fff;
+}
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,55 +1,74 @@
-import axios from 'axios';
+import axios from "axios";
 
 const state = {
-    todos: []
+  todos: [],
 };
 
 const getters = {
-    allTodos: (state) => {
-        return state.todos
-    }
+  allTodos: (state) => {
+    return state.todos;
+  },
 };
 
 const actions = {
+  async fetchTodos({ commit }) {
+    //We commit a mutation
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos"
+    );
+    commit("setTodos", response.data); //first param = mutation we want to call, second param = what we want to pass in
+  },
 
+  async addTodo({ commit }, title) {
+    const response = await axios.post(
+      "https://jsonplaceholder.typicode.com/todos",
+      {
+        title,
+        completed: false,
+      }
+    );
 
-    async fetchTodos({ commit }) {          //We commit a mutation
-        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
-        commit('setTodos', response.data) //first param = mutation we want to call, second param = what we want to pass in
-    },
+    commit("newTodo", response.data); //first param = mutation we want to call, second param = what we want to pass in
+  },
 
+  async deleteTodo({ commit }, id) {
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    commit("removeTodo", id);
+  },
 
-    async addTodo({ commit }, title) {
-        const response = await axios.post('https://jsonplaceholder.typicode.com/todos', {
-            title, completed: false
-        });
+  async filterTodos({ commit }, limit) {
+    //get selected number
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/todos?_limit=${limit}`
+    );
+    commit("setTodos", response.data);
+  },
 
-        commit('newTodo', response.data); //first param = mutation we want to call, second param = what we want to pass in
-    },
-
-    async deleteTodo({ commit }, id) {
-      await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
-      commit('removeTodo', id);
-       
-    },
-
-    async filterTodos({commit}, limit) {
-        //get selected number 
-const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`)
-        commit('setTodos', response.data) 
-    }
+  async updateTodo({ commit }, updTodo) {
+    const response = await axios.put(`https://jsonplaceholder.typicode.com/todos/${updTodo.id}`,
+    updTodo
+    );
+    commit("updateTodo", response.data);
+  },
 };
 
 const mutations = {
-    setTodos: (state, todos) => state.todos = todos,
-    newTodo: (state, todo) => state.todos.unshift(todo),
-    removeTodo: (state, id) => state.todos = state.todos.filter( todo => todo.id !== id )          //returns every todo out of state.todos in which the id is not the same. The matching id pair is NOT part of the filtered array
-    
-}; 
+  setTodos: (state, todos) => (state.todos = todos),
+  newTodo: (state, todo) => state.todos.unshift(todo),
+  removeTodo: (state, id) =>
+    (state.todos = state.todos.filter((todo) => todo.id !== id)), //returns every todo out of state.todos in which the id is not the same. The matching id pair is NOT part of the filtered array
+
+  updateTodo: (state, updTodo) => {
+    const index = state.todos.findIndex((todo) => todo.id === updTodo.id);
+    if (index !== -1) {
+      state.todos.splice(index, 1, updTodo);
+    }
+  },
+};
 
 export default {
-    state,
-    getters,
-    actions,
-    mutations
-}
+  state,
+  getters,
+  actions,
+  mutations,
+};
